66f42307a2f4a26ad4b48f5f7df670a6c488f1cd
root <root@capstone-laptop.neuwireless.club>
2016-10-25 14:37:19 -0400

    Been going through different parts of octomap and figuring out the funcitons

--- test1.cpp
+++ test1.cpp
@@ -1,22 +1,50 @@
-/*                    Written by Chris Canal
-This funciton takes the point2 cloud from the
+/*This funciton takes the point2 cloud from the
 Duo M and correctly updates the octomap */
 #include <octomap/octomap.h>
 
 using namespace std;
+#include <stdlib.h>     /* srand, rand */
 
+void createRandomPointCloud(octomap::Pointcloud & cloud9, int width, int hieght)
+{
+        float x = 1.0;
+        float y = 1.0;
+        float z = 1.0;
+        for (int i = 0 ; i < hieght ; i++)
+        {
+                for (int j = 0 ; j < width ; j++)
+		{
+			cloud9.push_back( x, y, z);
+        	        x += 1.0;
+                }
+		y += 1.0;
+        }
+}
 
 int main(){
-	octomap::OcTree chrisTree (0.1);
-	chrisTree.setOccupancyThres(0.5);
+        octomap::OcTree chrisTree (0.1);
+        chrisTree.setOccupancyThres(0.5);
         cout << "This shit is working" << endl;
-	
-	//Size of point2cloud = 640 x 480
-	octomap::Pointcloud sensorData;
-	float x = 1;
-	float y = 2;
-	float z = 3;
-	sensorData.push_back( x, y, z);
-	return 0;
 
+        //Size of pointCloud = 640 x 480
+        octomap::Pointcloud sensorData1;
+        createRandomPointCloud(sensorData1, 640, 480);
+	
+	//display point data in terminal
+	octomap::point3d nPoint;
+	for (int i = 0 ; i < 2500; i++)
+	{
+		nPoint = sensorData1.getPoint(i);	
+		cout << "x = " << nPoint.x() << "; y = " << nPoint.y() << "; z = " << nPoint.z() << ";\n"; 
+	}
+	//Pose6D(float x, float y, float z, double roll, double pitch, double yaw)
+	octomath::Pose6D startPose;        
+        octomath::Quaternion rotation1;
+	octomath::Vector3 translation1;
+	
+	rotation1 = startPose.rot();
+	translation1 = startPose.trans();
+	cout << "rotation = " << rotation1 << "; translation = " << translation1 << ";\n";
+	return 0;
+	
 }
