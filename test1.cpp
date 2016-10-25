af89b7f9898381b81b2fbaced10ffbbbbaf31476
root <root@capstone-laptop.neuwireless.club>
2016-10-25 15:46:27 -0400

    Finished low pass filter givenoctomap pointcloud

--- test1.cpp
+++ test1.cpp
@@ -21,7 +21,7 @@ void createRandomPointCloud(octomap::Pointcloud & cloud9, int width, int hieght)
         }
 }
 
-double getMiddleAverage(int totalW, int totalH, double percentSize)
+double getMiddleAverage(octomap::Pointcloud & sensorData, int totalW, int totalH, double percentSize)
 {
 	//using 1/10 of the field of view at the center
 	//in order to calculate the distance change
@@ -29,19 +29,23 @@ double getMiddleAverage(int totalW, int totalH, double percentSize)
 	//of points is streaming in from left to right
 	//and then from top to bottom, so it will have the
 	//entire first row first in order.
-	int right = (totalW*percentSize);
-	int left = (total);
-	for (int i = 0 ; i < hieght ; i++)
-        	{       
-                	for (int j = 0 ; j < width ; j++)
-                	{       
-                       		cloud9.push_back( x, y, z);
-                        	x += 1.0;
-                	} 
-                	y += 1.0;
+	octomap::point3d nPoint;
+	int right = (totalW*(percentSize/2)+(totalW/2));
+	int left = (totalW/2)-(totalW*(percentSize/2));
+	int top = (totalH*(percentSize/2)+(totalH/2));
+        int bottom = (totalH/2)-(totalH*(percentSize/2));
+	double count = 0.0;
+	double totalDepth = 0.0;
+	for (int i = left ; i <= right ; i++)
+        {	
+		for (int j = bottom ; j <= top ; j++)
+		{
+			nPoint = sensorData.getPoint((i+1)*(j+1)-1);
+			count += 1.0;
+			totalDepth += nPoint.z();
+		}
         }
-	
-	
+	return totalDepth/count;
 }
 	
 
@@ -69,6 +73,7 @@ int main(){
 	rotation1 = startPose.rot();
 	translation1 = startPose.trans();
 	cout << "rotation = " << rotation1 << "; translation = " << translation1 << ";\n";
+	cout << "Middle Pixels Average = " << getMiddleAverage(sensorData1, 640, 480, 0.1) << "\n";
 	return 0;
 	
 }
